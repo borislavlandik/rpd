c8e6561e5081a8a794a987cc3ae079fd520bc1e5
Borislav Landik <borislavlandik00@gmail.com>
2022-01-31 13:34:53 +0800

    feat: 🎸 add default styles

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,11 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <h1>Генератор рабочих программ дисциплин</h1>
+  <h2>Генератор рабочих программ дисциплин</h2>
+  <h3>Генератор рабочих программ дисциплин</h3>
+  <h4>Генератор рабочих программ дисциплин</h4>
+  <h5>Генератор рабочих программ дисциплин</h5>
+  <h6>Генератор рабочих программ дисциплин</h6>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
