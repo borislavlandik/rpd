9d9e697cb7eddfe2d50cda0cb2c6b349bb787c3f
Borislav Landik <borislavlandik00@gmail.com>
2022-02-03 16:20:48 +0800

    feat: 🎸 add icon component and svg sprite generator

---
+++ src/common/components/icon/rpd-icon.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+interface IIconProps {
+  iconName?: string;
+  width?: number;
+  height?: number;
+  left?: boolean;
+  right?: boolean;
+}
+
+withDefaults(defineProps<IIconProps>(), {
+  width: 24,
+  height: 24,
+});
+</script>
+
+<template>
+<svg
+  class="rpd-icon"
+  :class="[iconName, { 'rpd-icon_left': left }, { 'rpd-icon_right': right }]"
+  :width="width"
+  :height="height"
+>
+  <use
+    v-bind="{ 'xlink:href': require('@/assets/sprite.svg') + `#${iconName}` }"
+  >
+  </use>
+</svg>
+</template>
+
+<style scoped lang="scss">
+.rpd-icon_left {
+  margin-right: .5rem;
+}
+
+.rpd-icon_right {
+  margin-left: .5rem;
+}
+</style>
