8d1fb4a3b72a024aa6e4a444e1c3f676b3b73c5a
Borislav Landik <borislavlandik00@gmail.com>
2022-02-20 14:42:25 +0800

    feat: 🎸 add select component

---
+++ src/common/components/form/select/rpd-select.vue
@@ -0,0 +1,157 @@
+<script setup lang="ts">
+import Multiselect from '@vueform/multiselect';
+import { useVModel } from '@vueuse/core';
+import RpdIcon from '@/common/components/icon/rpd-icon.vue';
+import { pluralizeWithNumber } from '@/common/utils/pluralize';
+
+interface IFormInputProps {
+  modelValue?: string | string[];
+
+  options: string[] | { value: string; label: string; disabled?: boolean }[];
+
+  mode?: 'single' | 'multiple' | 'tags';
+  searchable?: boolean;
+  closeOnSelect?: boolean;
+  required?: boolean;
+  disabled?: boolean;
+  placeholder?: string;
+}
+
+interface IFromInputEmits {
+  (event: 'update:modelValue', data: string): void,
+}
+
+const props = withDefaults(defineProps<IFormInputProps>(), {
+  mode: 'single',
+  searchable: true,
+  closeOnSelect: true,
+  required: false,
+  disabled: false,
+});
+
+const emit = defineEmits<IFromInputEmits>();
+
+const value = useVModel(props, 'modelValue', emit);
+
+function defaultMultipleLabel(selectedValues: string[]): string {
+  return `Выбрано ${pluralizeWithNumber(selectedValues.length, ['значение', 'значения', 'значений'])}`;
+}
+
+</script>
+
+<template>
+  <div class="select-wrapper">
+    <label class="select-title">
+      <slot></slot>
+      <span class="select-title__required" v-show="required">*</span>
+    </label>
+    <span class="select">
+      <Multiselect
+        v-model="value"
+        :options="options"
+        :no-options-text="'Список пустой'"
+        :no-results-text="'Результатов не найдено'"
+        :hide-selected="false"
+        :multiple-label="defaultMultipleLabel"
+
+        :mode="mode"
+        :searchable="searchable"
+        :close-on-select="closeOnSelect"
+        :disabled="disabled"
+        :placeholder="placeholder"
+      >
+      <template v-slot:tag="{ option, handleTagRemove, disabled: disabledTag }">
+        <div class="tag">
+          <span class="tag__text">{{ option.label }}</span>
+          <rpd-icon
+            class="tag__clear-icon"
+            icon-name="ui-close"
+            v-if="!disabledTag"
+            :width="16"
+            :height="16"
+            @click.prevent="handleTagRemove(option, $event)"
+          ></rpd-icon>
+        </div>
+      </template>
+
+        <template v-slot:clear="{ clear }">
+          <rpd-icon
+            class="clear-icon"
+            icon-name="ui-close"
+            :width="24"
+            :height="24"
+            @click="clear"
+          ></rpd-icon>
+        </template>
+        <template v-slot:caret>
+          <rpd-icon
+            class="caret-icon"
+            icon-name="arrow-down"
+            :width="24"
+            :height="24"
+          ></rpd-icon>
+        </template>
+      </Multiselect>
+    </span>
+    <span class="select-hint">
+      <slot name="hint"></slot>
+    </span>
+  </div>
+</template>
+
+<style src="@/assets/styles/components/multiselect.scss" lang="scss"></style>
+
+<style scoped lang="scss">
+.select-wrapper {
+  display: inline-flex;
+  flex-direction: column;
+
+  min-width: 300px;
+}
+
+.select-title {
+  margin-left: 1rem;
+
+  font-weight: 700;
+}
+
+.select-title__required {
+  margin-left: -.25rem;
+}
+
+.select-hint {
+  margin-left: 1rem;
+
+  font-style: italic;
+  color: var(--text-colors-gray);
+}
+
+.select {
+  position: relative;
+}
+
+.clear-icon, .caret-icon {
+  z-index: 2;
+}
+
+.tag {
+  display: flex;
+  align-items: center;
+
+  max-width: 100%;
+
+  padding: .25rem .5rem;
+
+  border-radius: .25rem;
+  background-color: var(--ui-colors-blue-light);
+
+  color: var(--main-colors-white);
+}
+
+.tag__text {
+  max-width: calc(100% - 16px);
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+</style>
