1aeeb3cd104f5989b4d901fde7a4e3be8300c57e
Borislav Landik <borislavlandik00@gmail.com>
2022-02-23 19:41:10 +0800

    feat: 🎸 add transition for routes

--- src/App.vue
+++ src/App.vue
@@ -11,17 +11,20 @@ import RpdBreadcrumbs from '@/common/components/breadcrumbs/rpd-breadcrumbs.vue'
 
     <main class="app__main">
       <rpd-breadcrumbs class="app__breadcrumbs"></rpd-breadcrumbs>
-      <router-view v-slot="{ Component, route }">
-        <transition name="fade">
-          <component :is="Component" :key="route.path"></component>
-        </transition>
-      </router-view>
+      <div class="app__content">
+        <router-view class="app__router" v-slot="{ Component, route }">
+          <transition name="fade">
+            <component :is="Component" :key="route.path"></component>
+          </transition>
+        </router-view>
+      </div>
     </main>
   </div>
 </template>
 
 <style scoped lang="scss">
 $maxWidth: 1376px;
+
 .app__header-wrapper {
   display: flex;
   align-items: center;
@@ -45,8 +48,32 @@ $maxWidth: 1376px;
   margin: 0 auto;
 }
 
+.app__content {
+  position: relative;
+}
+
+.app__router {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+}
+
 .app__breadcrumbs {
   margin-top: 2rem;
   margin-bottom: 1rem;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition-property: opacity;
+  transition-duration: .2s;
+  transition-timing-function: ease-in-out;
+}
+
+.fade-enter-from,
+.fade-leave-active {
+  opacity: 0;
 }
 </style>
