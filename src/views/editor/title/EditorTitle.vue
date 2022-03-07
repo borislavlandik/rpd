0d3a37ecb2acce798ce58a6588996f1217bdb987
Borislav Landik <borislavlandik00@gmail.com>
2022-03-07 19:21:09 +0800

    feat: 🎸 add editor title view

--- src/views/editor/title/EditorTitle.vue
+++ src/views/editor/title/EditorTitle.vue
@@ -2,10 +2,17 @@
 import RpdButton from '@/common/components/button/rpd-button.vue';
 import RpdIcon from '@/common/components/icon/rpd-icon.vue';
 import RpdEditorHeaderHint from '@/common/components/editor/header-hint/rpd-editor-header-hint.vue';
+import RpdPreloader from '@/common/components/preloader/rpd-preloader.vue';
+
+import { joinLocal } from '@/common/utils/array-join-local';
+
+import useEditorTitle from './useEditorTitle';
+
+const { data, isLoading } = useEditorTitle();
 </script>
 
 <template>
-  <div>
+  <rpd-preloader :is-loading="isLoading">
     <rpd-editor-header-hint>
       <template v-slot:default>
         Титульный лист является первой страницей программы дисциплины <br>
@@ -25,49 +32,49 @@ import RpdEditorHeaderHint from '@/common/components/editor/header-hint/rpd-edit
       </template>
     </rpd-editor-header-hint>
 
-    <div class="content">
+    <div v-if="data" class="content">
       <h3>Общая информация о дисциплине</h3>
 
       <p>
         Название дисциплины:
-        <b>Информационные технологии в прогнозировании экономических процессов</b>
+        <b>{{ data.subject }}</b>
       </p>
 
       <p>
         Индекс дисциплины:
-        <b>Б1.В.ДВ.03.01</b>
+        <b>{{ data.index }}</b>
       </p>
 
       <p>
         Направление подготовки / специальность:
-        <b>09.03.03 Прикладная информатика</b>
+        <b>{{ data.specialty_code }} {{ data.specialty }}</b>
       </p>
 
       <p>
         Направленность (профиль) / специализация:
-        <b>Проектирование и разработка информационных систем</b>
+        <b>{{ data.specialty }}</b>
       </p>
 
       <p>
         Квалификация выпускника:
-        <b>Бакалавр</b>
+        <b>{{ data.degree }}</b>
       </p>
 
       <p>
         Форма обучения:
-        <b>очная</b>
+        <b class="lowercase">{{ data.study_form }}</b>
       </p>
 
       <p>
         Выпускающая кафедра:
-        <b>Кафедра алгебраических и информационных систем</b>
+        <b>{{ data.chair }}</b>
       </p>
 
-      <p>Курс(ы): <b>1 и 2</b></p>
-      <p>Семестр(ы): <b>1, 2 и 3</b></p>
-      <p>Учебный план набора <b>2022</b> года</p>
+      <p>Курс(ы): <b>{{ joinLocal(data.courses) }}</b></p>
+      <p>Семестр(ы): <b>{{ joinLocal(data.semestrs) }}</b></p>
+      <p>Учебный план набора <b>{{ data.year }}</b> года</p>
     </div>
-  </div>
+  </rpd-preloader>
 </template>
 
 <style scoped lang="scss">
